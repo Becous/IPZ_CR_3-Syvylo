2aed358b1b7edd98c870e1f4f4dac7c5bab5d64d
Becous <becousgame@gmail.com>
2024-02-24 12:32:12 +0200

    add contact item

--- app/src/main/java/ua/edu/lntu/cw3/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw3/MainActivity.kt
@@ -3,14 +3,29 @@ package ua.edu.lntu.cw3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ua.edu.lntu.cw3.ui.theme.IPZ_KR_3_Syvylo_RomanTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,17 +38,42 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ItemColumn()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Card(){
 
+
+@Composable
+fun ItemColumn(modifier: Modifier = Modifier) {
+    Card(
+        shape =  RoundedCornerShape(25.dp),
+        modifier = modifier){
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .background(color = Color.Yellow)
+                .fillMaxWidth()
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.image_1),
+                contentDescription = null,
+                modifier = Modifier
+                    .clip(RoundedCornerShape(50.dp))
+            )
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.padding(15.dp)) {
+                Text(text = "some text")
+                Spacer(modifier = Modifier.height(16.dp))
+                Text("some little text")
+            }
+
+        }
     }
 }
 
@@ -41,5 +81,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     IPZ_KR_3_Syvylo_RomanTheme {
+        ItemColumn()
     }
 }
